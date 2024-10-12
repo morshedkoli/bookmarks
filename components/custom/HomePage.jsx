217c334ef87bbbd518b6ef7b4c9e58daffe79991
morshedkoli <morshedkoli@gmail.com>
2024-10-13 00:45:05 +0600

    fix code with gemini

--- components/custom/HomePage.jsx
+++ components/custom/HomePage.jsx
@@ -20,7 +20,6 @@ export default function HomePage() {
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
   const { data, error } = useSWR("/api/category", fetcher);
-  console.log("data", data);
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
