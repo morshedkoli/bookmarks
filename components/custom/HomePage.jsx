298bb3d4d406592504f0caf8bb954e12e3fc1bc6
morshedkoli <morshedkoli@gmail.com>
2024-10-13 00:20:46 +0600

    first commit

---
+++ components/custom/HomePage.jsx
@@ -0,0 +1,63 @@
+"use client";
+import useSWR from "swr";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import Link from "next/link";
+import { Button } from "../ui/button";
+
+export default function HomePage() {
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+  const { data, error } = useSWR("/api/category", fetcher);
+  console.log("data", data);
+
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+      {data?.data?.map((category) => (
+        <div key={category.id} className="mb-4">
+          <Card className=" w-full  ">
+            <CardHeader>
+              <CardTitle>{category?.name} related website are here</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <Accordion type="single" collapsible className="w-full">
+                {category.websites.map((website) => (
+                  <AccordionItem key={website.id} value={website.id}>
+                    <AccordionTrigger>{website.name}</AccordionTrigger>
+                    <AccordionContent>{website.useFor}</AccordionContent>
+                    <AccordionContent>
+                      <Button asChild>
+                        <Link href={website.link}>Visit</Link>
+                      </Button>
+                    </AccordionContent>
+                  </AccordionItem>
+                ))}
+              </Accordion>
+            </CardContent>
+          </Card>
+        </div>
+      ))}
+      {/* {categories[data].map((category) => (
+        <Card className=" w-full  ">
+          <CardHeader>
+            <CardTitle>Notifications</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <Block />;
+          </CardContent>
+        </Card>
+      ))} */}
+    </div>
+  );
+}
---
+++ components/custom/MenubarApp.jsx
@@ -0,0 +1,46 @@
+const MenubarApp = () => {
+  return (
+    <>
+      <nav className="bg-black">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between h-16">
+            <div className="flex items-center">
+              <div className="flex-shrink-0">
+                <a
+                  href="https://facebook.com/murshedkoli"
+                  className="text-white"
+                >
+                  Murshed Al Main
+                </a>
+              </div>
+            </div>
+            <div className="hidden md:block">
+              <div className="ml-4 flex items-center space-x-4">
+                <a
+                  href="/"
+                  className="text-white hover:bg-white hover:text-black rounded-lg p-2"
+                >
+                  Home
+                </a>
+                <a
+                  href="/"
+                  className="text-white hover:bg-white hover:text-black rounded-lg p-2"
+                >
+                  Add Category
+                </a>
+                <a
+                  href="/"
+                  className="text-white hover:bg-white hover:text-black rounded-lg p-2"
+                >
+                  Add Website
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default MenubarApp;
