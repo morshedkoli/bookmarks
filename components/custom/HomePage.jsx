5a122369ee349c4f54dd3235108eeb535e4ee017
morshedkoli <morshedkoli@gmail.com>
2024-10-14 22:41:33 +0600

    update code help of gemini

--- components/custom/HomePage.jsx
+++ components/custom/HomePage.jsx
@@ -33,28 +33,9 @@ export default function HomePage() {
             <h2 className="text-2xl font-bold">{cat.name} related</h2>
             <hr />
             <div className="mt-5">
-              <Accordion type="single" collapsible className="w-full">
-                <AccordionItem value="item-1">
-                  <AccordionTrigger>Is it accessible?</AccordionTrigger>
-                  <AccordionContent>
-                    Yes. It adheres to the WAI-ARIA design pattern.
-                  </AccordionContent>
-                </AccordionItem>
-                <AccordionItem value="item-2">
-                  <AccordionTrigger>Is it styled?</AccordionTrigger>
-                  <AccordionContent>
-                    Yes. It comes with default styles that matches the other
-                    components&apos; aesthetic.
-                  </AccordionContent>
-                </AccordionItem>
-                <AccordionItem value="item-3">
-                  <AccordionTrigger>Is it animated?</AccordionTrigger>
-                  <AccordionContent>
-                    Yes. It&apos;s animated by default, but you can disable it
-                    if you prefer.
-                  </AccordionContent>
-                </AccordionItem>
-              </Accordion>
+              {cat.websites.map((website) => (
+                <h1>{website.name}</h1>
+              ))}
             </div>
           </div>
         ))}
